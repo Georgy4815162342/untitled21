cfac894e594b1fb3b78ed49632c2431d3d977967
Georgy4815162342 <164800642+Georgy4815162342@users.noreply.github.com>
2024-04-01 21:51:49 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -13,20 +13,16 @@ public class Main {
                 operIndex = i;
                 break;
             }
-
         }
         if (operIndex == 9) {
             System.out.println("Cтрока не является математической операцией");
             return;
         }
-
-
         String[] data = oper.split(operation2[operIndex]);
         if (data.length != 2) {
             System.out.println("Формат математической операции не удовлетворяет заданию - два операнда и один оператор");
             return;
         }
-
         if (data[0].contains("I") || data[0].contains("V") || data[0].contains("X")
                 || data[0].contains("1") || data[0].contains("2") || data[0].contains("3") || data[0].contains("4") || data[0].contains("5")
                 || data[0].contains("6") || data[0].contains("7") || data[0].contains("8") || data[0].contains("9")
@@ -45,7 +41,7 @@ public class Main {
                     (data[0].contains("1") || data[0].contains("2") | data[0].contains("3")
                             || data[0].contains("4") || data[0].contains("5") || data[0].contains("6") || data[0].contains("7") ||
                             data[0].contains("8") || data[0].contains("9"))) {
-                System.out.println("Значения должны быть в одном формате");
+                System.out.println("Используются одновременно разные системы счисления");
                 return;
             }
             int a, b;
@@ -57,21 +53,10 @@ public class Main {
                 a = Integer.parseInt(data[0]);
                 b = Integer.parseInt(data[1]);
             }
-            if (a>10||b>10){
-                System.out.println("iiii");
-            }
             if (oper.contains("1") || oper.contains("2") || oper.contains("3") || oper.contains("4") || oper.contains("5")
                     || oper.contains("6") || oper.contains("7") || oper.contains("8") || oper.contains("9")) {
-                String w = data[0];
-                String e = data[1];
-                char[] strToArray = w.toCharArray();
-                if (strToArray.length > 1) {
-                    System.out.println("Вводите значения меньше 10");
-                    return;
-                }
-                char[] strToArray1 = e.toCharArray();
-                if (strToArray1.length > 1) {
-                    System.out.println("Вводите значения меньше 10");
+                if (a>10||b>10){
+                    System.out.println("Вводите значения не больше 10 ");
                     return;
                 }
                 int result;
@@ -94,8 +79,8 @@ public class Main {
                 }
             } else if (data[0].contains("I") || data[0].contains("V") || data[0].contains("X") &&
                     data[1].contains("I") || data[1].contains("V") || data[1].contains("X")) {
-                if (a > 9 || b > 9) {
-                    System.out.println("Вводите значения меньше X");
+                if (a > 10 || b > 10) {
+                    System.out.println("Вводите значения не более X");
                     return;
                 }
             }
@@ -117,8 +102,8 @@ public class Main {
                 resultR = a / b;
                 System.out.println(converter.intToRoman(resultR));
             }
-
         }else{
             System.out.println("Вы ввели некорректное значение");
             return;
         }}}
+
